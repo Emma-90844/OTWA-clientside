200bced01914c6a093987da333357e1207ac5698
Emma-90844 <enabojohn@gmail.com>
2022-08-07 21:02:08 +0300

    Structured the home component

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import "./home.css";
+import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
+
+export const Home = () => {
+  return (
+    <div className="home">
+      <Navbar />
+      <Header />
+    </div>
+  );
+};

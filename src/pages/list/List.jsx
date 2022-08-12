7e465419f561d46a339a007a2588baba1ce6adf6
Emma-90844 <enabojohn@gmail.com>
2022-08-12 14:13:58 +0300

    Small edit to allow deploy

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -6,11 +6,13 @@ import { useState } from "react";
 import { format } from "date-fns";
 import { DateRange } from "react-date-range";
 import SearchItem from "../../components/searchItem/SearchItem";
+
+
 const List = () => {
   const location = useLocation();
-  const [destination, setDestination] = useState(location.state.destination);
+  const [destination] = useState(location.state.destination);
   const [date, setDate] = useState(location.state.date);
-  const [options, setOptions] = useState(location.state.options);
+  const [options] = useState(location.state.options);
   const [openDate, setOpenDate] = useState(false);
   return (
     <div>
@@ -49,59 +51,67 @@ const List = () => {
             {/* item 1 */}
             <div className="lsItem">
               <label htmlFor="">Options</label>
-            <div className="lsOptions">
-                   {/* smaller item */}
-              <div className="lsOptionItem">
-                <span className="lsOptionText">
-                  Min Price <small>per night</small>
-                </span>
-                <input type="number" className="lsOptionInput"></input>
+              <div className="lsOptions">
+                {/* smaller item */}
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">
+                    Min Price <small>per night</small>
+                  </span>
+                  <input type="number" className="lsOptionInput"></input>
+                </div>
+
+                {/* smaller item */}
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">
+                    Max Price <small>per night</small>
+                  </span>
+                  <input type="number" className="lsOptionInput"></input>
+                </div>
+
+                {/* smaller item */}
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Adults</span>
+                  <input
+                    type="number"
+                    min={1}
+                    placeholder={options.adult}
+                    className="lsOptionInput"
+                  ></input>
+                </div>
+
+                {/* smaller item */}
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Children</span>
+                  <input
+                    type="number"
+                    min={0}
+                    placeholder={options.children}
+                    className="lsOptionInput"
+                  ></input>
+                </div>
+
+                {/* smaller item */}
+                <div className="lsOptionItem">
+                  <span className="lsOptionText">Room </span>
+                  <input
+                    type="number"
+                    min={1}
+                    placeholder={options.room}
+                    className="lsOptionInput"
+                  ></input>
+                </div>
               </div>
-
-              {/* smaller item */}
-              <div className="lsOptionItem">
-                <span className="lsOptionText">
-                  Max Price <small>per night</small>
-                </span>
-                <input type="number" className="lsOptionInput"></input>
-              </div>
-
-              {/* smaller item */}
-              <div className="lsOptionItem">
-                <span className="lsOptionText">Adults</span>
-                <input type="number" min={1} placeholder={options.adult} className="lsOptionInput"></input>
-              </div>
-
-              {/* smaller item */}
-              <div className="lsOptionItem">
-                <span className="lsOptionText">Children</span>
-                <input type="number" min={0} placeholder={options.children} className="lsOptionInput"></input>
-              </div>
-
-              {/* smaller item */}
-              <div className="lsOptionItem">
-                <span className="lsOptionText">Room </span>
-                <input type="number" min={1} placeholder={options.room} className="lsOptionInput"></input>
-              </div>
-
-
-            </div>
-            
-            
-           
-
             </div>
             <button>Search</button>
           </div>
-
           {/* LEFT SIDE. */}
           <div className="listResult">
-            <SearchItem/>
-            <SearchItem/>
-            <SearchItem/>
-            <SearchItem/>
-            <SearchItem/>
-            <SearchItem/>
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
+            <SearchItem />
           </div>
         </div>
       </div>
